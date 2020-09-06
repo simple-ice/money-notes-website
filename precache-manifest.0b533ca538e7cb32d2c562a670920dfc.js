d9d532430b6807d7dee17cbcab5f4fa1608ce019
simple-ice <simplewu233@gmail.com>
2020-09-06 11:23:35 +0800

    deploy

---
+++ precache-manifest.0b533ca538e7cb32d2c562a670920dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81088a3841999c5ce6f",
+    "url": "/money-notes-website/css/app.e5264eb8.css"
+  },
+  {
+    "revision": "b4b89f2becff003694fd91bc9e9af615",
+    "url": "/money-notes-website/index.html"
+  },
+  {
+    "revision": "e81088a3841999c5ce6f",
+    "url": "/money-notes-website/js/app.29488c42.js"
+  },
+  {
+    "revision": "546afaed703982200134",
+    "url": "/money-notes-website/js/chunk-vendors.5dcd6a6e.js"
+  },
+  {
+    "revision": "13b537c20b5563a961ccaadae4406974",
+    "url": "/money-notes-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-notes-website/robots.txt"
+  }
+]);
